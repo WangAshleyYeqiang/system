0bd7885e684861ff3580ebc6e3acb6c590cb7ac6
Tintin <Tintin@example.com>
2023-09-18 16:33:12 +0800

    新建home页

---
+++ src/components/Hall.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Hall"
+}
+</script>
+
+<style>
+
+</style>
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -2,15 +2,15 @@
   <div class="">
   
     Sign In
-    <el-form :model="ruleForm" status-icon:rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-      <el-form-item label="UserID" prop="userid">
-        <el-input type="text" v-model="ruleForm.userid" autocomplete="off"></el-input>
+    <el-form :model="form" :rules="rules" ref="form">
+      <el-form-item label="UserID" prop="userID">
+        <el-input type="text" v-model="form.userID" ></el-input>
       </el-form-item>
-      <el-form-item label="Password" prop="pass">
-        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      <el-form-item label="Password" prop="userPassword">
+        <el-input type="password" v-model="form.userPassword"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">Submit</el-button>
+        <el-button type="primary" @click="submitForm('form')">Submit</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -23,62 +23,50 @@ import hashCode from "./../utils/hash"
 export default {
   name: 'SignIn',
   data() {
-    var validatePass = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("Please enter your password"));
-      } else {
-        if (this.ruleForm.checkPass !== "") {
-          this.$refs.ruleForm.validateField("checkPass");
-        }
-        callback();
-      }
-    };
 
-    var validateUserid = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("The user id cannot be empty"));
-      } else {
-        callback();
-      }
-    };
 
     return {
-      ruleForm: {
-        pass: "",
-        userid: "",
+      form: {
+        userPassword: "",
+        userID: "",
       },
       rules: {
-        pass: [{ validator: validatePass, trigger: "blur" }],
-        userid: [{ validator: validateUserid, trigger: "blur" }],
+        userPassword: [{ required:true, trigger: "blur" }],
+        userID: [{ required:true, trigger: "blur" }],
       },
     };
   },
 
   methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          axios.get("http://localhost:8081/userInfo/getUserInfoByUserID?userID="+this.ruleForm.userid).then(res=>{
-            console.log(res)
-            if(res.data.length==0){
-              console.log(this.ruleForm.userid+' Not Found')
-              alert(this.ruleForm.userid+' Not Found');
-            }else{
-              if(hashCode(this.ruleForm.pass)==res.data.userPassword){
-                console.log('Log In Success')
-                alert(`${res.data.userName}(${res.data.userID}) Log In Success`);
-              }else{
-                console.log("Password Error")
-                alert("Password Error")
-              }
-            }
-          })
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
+    submitForm(){
+      // this.$router.push('/home')
+      console.log(this.form.userID)
     },
+    // submitForm1(formName) {
+    //   this.$refs[formName].validate((valid) => {
+    //     if (valid) {
+    //       axios.get("http://localhost:8081/userInfo/getUserInfoByUserID?userID="+this.ruleForm.userid).then(res=>{
+    //         console.log(res)
+    //         if(res.data.length==0){
+    //           console.log(this.ruleForm.userid+' Not Found')
+    //           alert(this.ruleForm.userid+' Not Found');
+    //         }else{
+    //           if(hashCode(this.ruleForm.pass)==res.data.userPassword){
+    //             console.log('Log In Success')
+    //             // this.$router.push('/home')
+    //             alert(`${res.data.userName}(${res.data.userID}) Log In Success`);
+    //           }else{
+    //             console.log("Password Error")
+    //             alert("Password Error")
+    //           }
+    //         }
+    //       })
+    //     } else {
+    //       console.log("error submit!!");
+    //       return false;
+    //     }
+    //   });
+    // },
   },
 }
 </script>
