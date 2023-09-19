8d960749f68f0210648bfdc8058e50311e239e37
Zero <Zero@example.com>
2023-09-19 10:43:22 +0800

    login页面样式编写

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,17 +3,8 @@
   <div class="bg">
     <h1 class="text1">Academic Affairs System of Hainan University</h1>
     <h1 class="text2">Already Signed up?</h1>
-    <h1 class="text3">Sign Up for an Account </h1>
-    <h1 class="text4">Let's get you all set up so you can start creating your first admission experience</h1>
-    <label class="text5">Name</label>
-    <label class="text6">Student Number</label>
-    <label class="text7">Password</label>
-    <el-input  class="el_input_name"  v-model="input" placeholder="Please enter the name"></el-input>
-    <el-input  class="el_input_number" v-model="input" placeholder="Please enter the Student Number" type="number"></el-input>
-    <el-input  class="el_input_password" v-model="input" placeholder="Please enter the Password" type="password"></el-input>
 
-  
-    <el-button class="el_button_login" style="color: rgba(255, 255, 255, 1); background-color: rgba(53,87,220,1);">Log In</el-button>
+    <el-button @click="register_onClick" class="el_button_login" style="color: rgba(255, 255, 255, 1); background-color: rgba(53,87,220,1);">Register</el-button>
 
 
     <router-link to='/login/register'>Register</router-link>  <br>
@@ -30,13 +21,18 @@
 <script>
   import SignIn from '@/components/SignIn.vue';
   export default {
-    data() {
+  
+    name: 'Login',
+      data() {
     return {
       input: ''
     }
   },
-    name: 'Login',
-    
+  methods:{
+    register_onClick(){
+      this.$router.push('/login/register');
+    }
+  }
   };
   
 </script>
@@ -71,71 +67,6 @@
   font-weight: 700;
   color: rgba(255, 255, 255, 1);
   position: absolute;
-}
-.text3{
-  left: 900px;
-  top: 60px;
-  width: 271px;
-  height: 39px;
-  font-size: 20px;
-  font-weight: 700;
-  color: rgba(32, 40, 66, 1);
-  position: absolute;
-}
-.text4{
-  left: 855px;
-  top: 100px;
-  width: 363px;
-  height: 55px;
-  font-size: 14px;
-  font-weight: 400;
-  color: rgba(32, 40, 66, 1);
-  position: absolute;
-}
-.text5{
-  left: 650px;
-  top: 180px;
-  width: 387px;
-  height: 68px;
-  font-size: 14px;
-  font-weight: 600;
-  color: rgba(32, 40, 66, 1);
-  position: absolute;
-}
-.text6{
-  left: 687px;
-  top: 300px;
-  width: 387px;
-  height: 68px;
-  font-size: 14px;
-  font-weight: 600;
-  color: rgba(32, 40, 66, 1);
-  position: absolute;
-}
-.text7{
-  left: 668px;
-  top: 420px;
-  width: 387px;
-  height: 68px;
-  font-size: 14px;
-  font-weight: 600;
-  color: rgba(32, 40, 66, 1);
-  position: absolute;
-}
-.el_input_name{
-  left: 750px;
-  top: 200px;
-  max-width: 400px;
-}
-.el_input_number{
-  left: 352px;
-  top: 320px;
-  max-width: 400px;
-}
-.el_input_password{
-  left:-47px;
-  top: 440px;
-  max-width: 400px;
 }
 .el_button_login{
   left: 225px;
