78782c67562144fd22b578bfa3acd2ffc26c79b4
unknown <you@example.com>
2023-09-22 21:06:06 +0800

    111

--- src/components/My.vue
+++ src/components/My.vue
@@ -1,51 +1,34 @@
 <template>
-  <!-- <div>
-    <div>Personal information</div>
-    <br />
-
-    <div class="">
-      userName: {{$store.state.userInfo.userName}}
-    </div>
-
-    <div class="">
-      userGender: {{$store.state.userInfo.Gender}}
-    </div>
-    
-    <div class="">
-      userPhone: {{$store.state.userInfo.userPhone}}
-    </div>
-
-    <div class="">
-      userLastLoginTime: {{$store.state.userInfo.userLastLoginTime}}
-    </div>
-
-    <div class="">
-      userCreateTime: {{$store.state.userInfo.userCreateTime}}
-    </div>
-
-    <div class="">
-      userPassword: {{$store.state.userInfo.userPassword}}
-    </div>
-
-    <div class="">
-      userPrivilege: {{$store.state.userInfo.userPrivilege}}
-    </div>
-
-    <div class="">
-      userType: {{$store.state.userInfo.userType}}
-    </div> -->
 
 
  <el-descriptions title="Personal Information" column="2" border>
-  <el-descriptions-item label="userID"></el-descriptions-item>
-  <el-descriptions-item label="userName">{{$store.state.userInfo.userName}}</el-descriptions-item>
-  <el-descriptions-item label="userGender"></el-descriptions-item>
-  <el-descriptions-item label="userPhone"></el-descriptions-item>
-  <el-descriptions-item label="userLastLoginTime"></el-descriptions-item>
-  <el-descriptions-item label="userCreateTime"></el-descriptions-item>
-  <el-descriptions-item label="userPassword"></el-descriptions-item>
-  <el-descriptions-item label="userPrivilege"></el-descriptions-item>
-  <el-descriptions-item label="userType"></el-descriptions-item>
+  <el-descriptions-item label="user ID">
+    {{$store.state.userInfo.userID}}
+  </el-descriptions-item>
+  <el-descriptions-item label="Name">
+    {{$store.state.userInfo.userName}}
+  </el-descriptions-item>
+  <el-descriptions-item label="Gender">
+    {{$store.state.userInfo.userGender}}
+  </el-descriptions-item>
+  <el-descriptions-item label="Phone">
+    {{$store.state.userInfo.userPhone}}
+  </el-descriptions-item>
+  <el-descriptions-item label="Last LoginTime">
+    {{$store.state.userInfo.userLastLoginTime}}
+  </el-descriptions-item>
+  <el-descriptions-item label="Create Time">
+    {{$store.state.userInfo.userCreateTime}}
+  </el-descriptions-item>
+  <el-descriptions-item label="Password">
+    {{$store.state.userInfo.userPassword}}
+  </el-descriptions-item>
+  <el-descriptions-item label="Privilege">
+    {{$store.state.userInfo.userPrivilege}}
+  </el-descriptions-item>
+  <el-descriptions-item label="user Type">
+    {{$store.state.userInfo.userType}}
+  </el-descriptions-item>
 </el-descriptions>
 
 
