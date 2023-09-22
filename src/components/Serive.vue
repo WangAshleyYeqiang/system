6df47c15f21c34cf1923982be1cbf3a8c32b97ad
Tintin <Tintin@example.com>
2023-09-22 22:50:14 +0800

    增加了权限相关逻辑修改

--- src/components/Serive.vue
+++ src/components/Serive.vue
@@ -17,7 +17,7 @@
               </ul>
               
             </div>
-            <el-button type="primary" plain style="align-items: center;  width:150px; margin-top: 50px; margin-left: -35px;">Buy It</el-button>
+            <el-button @click="upLevel(2)" type="primary" plain style="align-items: center;  width:150px; margin-top: 50px; margin-left: -35px;">Buy It</el-button>
           </div>
 
         </el-col>
@@ -34,7 +34,7 @@
               </ul>
               
             </div>
-            <el-button type="primary" plain style="align-items: center;  width:150px; margin-top: 50px; margin-left: -10px;">Buy It</el-button>
+            <el-button @click="upLevel(4)" :disabled="$store.state.userInfo.userPrivilege==4?true:false" type="primary" plain style="align-items: center;  width:150px; margin-top: 50px; margin-left: -10px;">{{$store.state.userInfo.userPrivilege==4?'You have it':'Buy it'}}</el-button>
           </div>
 
         </el-col>
@@ -51,7 +51,7 @@
               </ul>
               
             </div>
-            <el-button type="primary" plain style="align-items: center; width:150px; margin-top: 50px; margin-left: -30px;">Buy It</el-button>
+            <el-button @click="upLevel(3)" type="primary" plain style="align-items: center; width:150px; margin-top: 50px; margin-left: -30px;">Buy It</el-button>
           </div>
 
         </el-col>
@@ -66,8 +66,43 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: "Serive"
+  name: "Serive",
+  methods: {
+    upLevel(kinds){
+
+
+
+
+
+      var userInfo=this.$store.state.userInfo;
+      userInfo.userPrivilege=parseInt(kinds)
+      axios.put("http://localhost:8081/userInfo/updateUserInfoByUserID",userInfo ).then(res=>{
+        console.log(res);
+        if (res.data){
+          this.$message({
+            message:"Successfully up to Level VIP"+(parseInt(kinds)-2).toString(),
+            type: 'success'
+          })
+
+          axios.get("http://localhost:8081/userInfo/getUserInfoByUserID?userID="+this.$store.state.userInfo.userID).then(ress=>{
+            console.log(ress)
+            this.$store.state.userInfo=ress.data;
+            this.$cookies.set("userInfo", ress.data, { expires: 7 })
+          })
+        }
+      }).catch(err=>{
+        this.$message({
+            message:"Fail up to Level VIP"+(parseInt(kinds)-2).toString(),
+            type: 'warning'
+          })
+      })
+        //this.$store.state.userInfo.userPrivilege=kinds;
+        
+        return
+    }
+  },
 }
 </script>
 
