e01b4598d21362f8db9ebf7c70028a8da6df9118
Xiaohaohao <Xiaohaohao@example.com>
2023-09-22 16:43:52 +0800

    Update My.vue

--- src/components/My.vue
+++ src/components/My.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
     My
+
+    <el-button type="danger" plain @click="signout_onClick">Sign out</el-button>
   </div>
 </template>
 
 <script>
 export default {
-    name:"My"
+    name:"My",
+    
+    methods: {
+      signout_onClick(){
+        this.$cookies.remove('userInfo')
+        this.$store.state.userInfo={userID:'',userName:'',userGender:'',userPhone:'',userSchool:'',userLastLoginTime:'',userCreateTime:'',userPassword:'',userPrivilege:'',userType:'',token:'',}
+        this.$message({
+          message:'Log out success',
+          type:'success'
+        })
+        this.$router.push('/login/signin')
+      },
+    }
 }
+
 </script>
 
 <style>
